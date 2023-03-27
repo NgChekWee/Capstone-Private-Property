3a40fed94fd1ad4a41448ac5129576c9fb6fa570
NgChekWee <127093470+NgChekWee@users.noreply.github.com>
2023-03-27 10:03:57 +0800

    Updates
    1.Update app file
    2. Added report

--- prop_app.py
+++ prop_app.py
@@ -20,27 +20,34 @@ rental["Date"]=pd.to_datetime(rental["Date"])
 
 # Set input widgets
 st.sidebar.subheader('Select property attributes')
-area = st.sidebar.number_input('Area (sqft)', 400, 5000)
-property_type = st.sidebar.slider('Property Type (Apartment = 1 ; Condominium = 2)', 1, 2)
-tenure = st.sidebar.slider('Tenure (99-leasehold = 1 ; Freehold = 2)', 1, 2)
-unit_age = st.sidebar.slider('Age of unit (years)', 0, 10, 50)
-dist_to_mrt = st.sidebar.number_input('Distance to MRT (meters)', 0, 3000)
-listing_price = st.sidebar.number_input('Unit Listing Price (psf)', 0, 6000)
+area = st.sidebar.number_input('Area (sqft)', min_value=400, max_value=5000,value=1200)
+property_type = st.sidebar.slider('Property Type (Apartment = 1 ; Condominium = 2)', min_value=1, max_value=2,value=2)
+tenure = st.sidebar.slider('Tenure (99-leasehold = 1 ; Freehold = 2)', min_value=1, max_value=2, value=2)
+unit_age = st.sidebar.slider('Age of unit (years)', min_value=0, max_value=50, value=10 )
+dist_to_mrt = st.sidebar.number_input('Distance to MRT (meters)', min_value=0, max_value=3000, value=500)
+listing_price = st.sidebar.number_input('Unit Listing Price (psf)', min_value=0, max_value=6000, value=2000)
 
 # Generate prediction based on user selected attributes
 y_pred = round(((0.2964 * area) - (89.3533 * property_type) + (384.7917 * tenure) - (74.8624 * unit_age) - (0.0396 * dist_to_mrt) + 2061.99),2)
-valuation = round(((listing_price-y_pred)/y_pred *100),2)
+valuation = round(((y_pred-listing_price)/listing_price *100),2)
 
 # Display EDA
 st.subheader('Exploratory Data Analysis')
-st.write('Mean resale prices, transaction volumes and mean rental in psf of private properties in CCR from 2018 to 2022')
 
 prices_grp = prices.groupby(by="Sale_Date")["Unit_Price"].mean()
 vol_grp = prices.groupby(by="Sale_Date")["Unit_Price"].count()
 rental_grp=rental.groupby(by="Date")["Rental psf"].mean()
 
+st.write("")
+st.write('Mean resale prices in psf of private properties in CCR from 2018 to 2022')
 st.line_chart(prices_grp)
+
+st.write("")
+st.write('Transaction volumes of private properties in CCR from 2018 to 2022')
 st.bar_chart(vol_grp)
+
+st.write("")
+st.write('Mean rental in psf of private properties in CCR from 2018 to 2022')
 st.line_chart(rental_grp)
 
 # Print input features
